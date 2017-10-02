88318062d61845051c06c30d8ca5958e4f40cc5b
Michael Barz <mbarz6@gmail.com>
2017-10-01 22:31:17 -0500

    chapter 2

--- foundations/insertion_sort.py
+++ foundations/insertion_sort.py
@@ -8,7 +8,24 @@ def insertion_sort(numbers):
 		key = numbers[j]
 		# insert numbers[j] into the sorted sequence numbers[0, ..., j-1]
 		i = j - 1
-		while i > 0 and numbers[i] > key:
+		while i >= 0 and numbers[i] > key:
+			numbers[i+1] = numbers[i]
+			i = i - 1
+		numbers[i+1] = key
+	return numbers
+
+
+""" 
+
+Takes a list of numbers as input, and outputs a permutation of that list which is sorted (in **descending** order).
+
+"""
+def insertion_sort2(numbers):
+	for j in range(1, len(numbers)): # purposely skipping first entry of numbers
+		key = numbers[j]
+		# insert numbers[j] into the sorted sequence numbers[0, ..., j-1]
+		i = j - 1
+		while i >= 0 and numbers[i] < key:
 			numbers[i+1] = numbers[i]
 			i = i - 1
 		numbers[i+1] = key
